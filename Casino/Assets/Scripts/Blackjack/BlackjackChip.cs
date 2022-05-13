37dea6e79270c22f07c611e26092a28065b29961
Supetorus <66396555+Supetorus@users.noreply.github.com>
2022-05-12 19:06:14 -0600

    Bank works

--- Casino/Assets/Scripts/Blackjack/BlackjackChip.cs
+++ Casino/Assets/Scripts/Blackjack/BlackjackChip.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Chip : MonoBehaviour
+public class BlackjackChip : MonoBehaviour
 {
 	public int value;
 	public BlackjackManager blackjackManager;
