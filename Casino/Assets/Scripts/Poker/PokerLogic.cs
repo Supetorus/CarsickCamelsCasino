b39445fdebed41d6534e094c4e3263f8d2fde840
Sherrlocke <78228547+Sherrlocke@users.noreply.github.com>
2022-06-02 15:51:37 -0600

    Poker :)
    it work now

--- Casino/Assets/Scripts/Poker/PokerLogic.cs
+++ Casino/Assets/Scripts/Poker/PokerLogic.cs
@@ -52,8 +52,8 @@ public class PokerLogic : MonoBehaviour
         cardManager.Deal(5, true, playerHand);
 
         foreach (Transform card in playerHandLocation.transform) Destroy(card.gameObject);
-
-        if (playerDebugHand != null) playerHand = playerDebugHand;
+        List<Card> debugHang = playerHand;
+        //if (playerDebugHand != null) playerHand = playerDebugHand;
 
         DisplayCards();
     }
